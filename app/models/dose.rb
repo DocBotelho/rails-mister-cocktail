a2faf42ef738e85a5796f1ebbdf13d1c92ab9e78
Claudio Botelho <claudio.botelho@smartificar.com.br>
2017-08-03 18:05:17 -0300

    rake all green. Need to fix page errors when cocktail doses = nil

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,5 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :name, uniqueness: true, presence: true
+  validates :description, presence: true
+  validates :ingredient, presence: true
+  validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :cocktails
+  resources :cocktails do
+    resources :doses, only: [:index, :new, :show, :create]
+  end
 
+  resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
